77f0f7347a8dde96c9c4845c95a55cf54825172a
성영 <tjddud0196@gmail.com>
2026-01-20 23:23:01 +0900

    config 수정

--- config/config.js
+++ config/config.js
@@ -8,15 +8,8 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: 'mysql',
   },
-  test: {
-    username: "root",
-    password: process.env.DB_PASSWORD,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql"
-  },
   production: {
-    username: "root",
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
